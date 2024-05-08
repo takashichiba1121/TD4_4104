507d815fee099c654f65e23a0c9915b9f05526a7
keita_momiyama <93181795+keita034@users.noreply.github.com>
2024-05-08 16:50:02 +0900

    仮描画

--- Editor/Source/Scene/EditorScene.cpp
+++ Editor/Source/Scene/EditorScene.cpp
@@ -37,21 +37,25 @@ void EditorScene::Initialize(int32_t screen)
 		editorMap[ i ].resize(mapSize_.x);
 	}
 
-	noneGraphHandle = LoadGraph("Resource/NoneChip");
-	roadGraphHandle = LoadGraph("Resource/RoadChip");
-	doorGraphHandle = LoadGraph("Resource/DoorChip");
-	roomGraphHandle = LoadGraph("Resource/RoomChip");
-	lockroomGraphHandle = LoadGraph("Resource/LockroomChip");
+	noneGraphHandle_ = LoadGraph("Resource/NoneChip.png");
+	roadGraphHandle_ = LoadGraph("Resource/RoadChip.png");
+	doorGraphHandle_ = LoadGraph("Resource/DoorChip.png");
+	roomGraphHandle_ = LoadGraph("Resource/RoomChip.png");
+	lockroomGraphHandle_ = LoadGraph("Resource/LockroomChip.png");
 }
 
 void EditorScene::Update()
 {
 	screenOldMousePos_ = screenMousePos_;
+	mouseInput_ = GetMouseInput();
 
 	int mouseWheelRotVol = GetMouseWheelRotVol();
 
 	scale_.x += 0.1f * mouseWheelRotVol;
 	scale_.y += 0.1f * mouseWheelRotVol;
+
+	max(scale_.x,scale_.x,0.0000001f);
+	max(scale_.y,scale_.y,0.0000001f);
 
 	GetMousePoint(&screenMousePos_.x,&screenMousePos_.y);
 
@@ -59,9 +63,14 @@ void EditorScene::Update()
 
 	editorMousePos_ = GetEditorMousePos();
 
-	if ( IsEditorMapWithin(editorMousePos_.x/blockSize_,editorMousePos_.y/blockSize_) )
-	{
+	selectChip_ = ChipIndex::ROAD;
 
+	if ( IsEditorMapWithin(editorMousePos_.x / GetBlockSize(),editorMousePos_.y / GetBlockSize()) )
+	{
+		if ( mouseInput_  )
+		{
+			editorMap[ editorMousePos_.y / GetBlockSize() ][ editorMousePos_.x / GetBlockSize() ] = selectChip_;
+		}
 	}
 
 }
@@ -77,17 +86,22 @@ void EditorScene::Draw()
 			int32_t centerX = blockSizeHalf + blockSize_ * j;
 			int32_t centerY = blockSizeHalf + blockSize_ * i;
 
-			ChipDraw(centerX,centerY,editorMap[i][j]);
+			ChipDraw(centerX,centerY,editorMap[ i ][ j ]);
 			DrawBoxAA(( centerX - blockSizeHalf ),( centerY - blockSizeHalf ),( centerX + blockSizeHalf ),( centerY + blockSizeHalf ),0xffffffff,false);
 		}
 	}
 
-	DrawBoxAA(( editorMousePos_.x / GetBlockSize() ) * 32,( editorMousePos_.y / GetBlockSize() ) * 32,( editorMousePos_.x / GetBlockSize() ) * 32 + 32,( editorMousePos_.y / GetBlockSize() ) * 32 + 32,0xffffffff,true);
+	ChipDraw(( editorMousePos_.x / GetBlockSize() ) * 32,( editorMousePos_.y / GetBlockSize() ) * 32,selectChip_,0);
+
+	//DrawBoxAA(
+	// ( editorMousePos_.x / GetBlockSize() ) * 32,( editorMousePos_.y / GetBlockSize() ) * 32,
+	// ( editorMousePos_.x / GetBlockSize() ) * 32 + 32,( editorMousePos_.y / GetBlockSize() ) * 32 + 32
+	// ,0xffffffff,true);
 }
 
 void EditorScene::UIUpdate()
 {
-	ImGui::Begin("EditorView",nullptr,ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+	ImGui::Begin("EditorView",nullptr,ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar);
 	EditorView();
 	ImGui::End();
 
@@ -119,7 +133,7 @@ float2 EditorScene::GetScale()
 	float2 ret =
 	{
 		 scale_.x / INIT_SCALE.x ,
-		 scale_.y / INIT_SCALE.y 
+		 scale_.y / INIT_SCALE.y
 	};
 
 	return ret;
@@ -135,7 +149,7 @@ void EditorScene::EditorView()
 
 void EditorScene::EditorMove()
 {
-	if ( CheckHitKey(KEY_INPUT_LSHIFT) && GetMouseInput() & MOUSE_INPUT_LEFT )
+	if ( CheckHitKey(KEY_INPUT_LSHIFT) && mouseInput_ & MOUSE_INPUT_LEFT )
 	{
 		int2 mouseMove = screenOldMousePos_ - screenMousePos_;
 
@@ -168,27 +182,27 @@ int2 EditorScene::GetEditorMousePos()
 
 bool EditorScene::IsEditorMapWithin(int32_t x,int32_t y)
 {
-	return x != -1 && y != -1 && x < mapSize_.x&& y < mapSize_.y;
+	return x >= 0 && y >= 0 && x < mapSize_.x && y < mapSize_.y;
 }
 
-void EditorScene::ChipDraw(size_t x,size_t y,int8_t chip)
+void EditorScene::ChipDraw(size_t x,size_t y,int8_t chip,int32_t sign)
 {
 	switch ( chip )
 	{
 	case NONE:
-		DrawGraph(x+ blockSizeHalf_,y+ blockSizeHalf_,noneGraphHandle,true);
+		DrawGraph(x + ( blockSizeHalf_ * sign ),y + ( blockSizeHalf_ * sign ),noneGraphHandle_,true);
 		break;
 	case ROAD:
-		DrawGraph(x + blockSizeHalf_,y + blockSizeHalf_,roadGraphHandle,true);
+		DrawGraph(x + ( blockSizeHalf_ * sign ),y + ( blockSizeHalf_ * sign ),roadGraphHandle_,true);
 		break;
 	case DOOR:
-		DrawGraph(x + blockSizeHalf_,y + blockSizeHalf_,doorGraphHandle,true);
+		DrawGraph(x + ( blockSizeHalf_ * sign ),y + ( blockSizeHalf_ * sign ),doorGraphHandle_,true);
 		break;
 	case ROOM:
-		DrawGraph(x + blockSizeHalf_,y + blockSizeHalf_,roomGraphHandle,true);
+		DrawGraph(x + ( blockSizeHalf_ * sign ),y + ( blockSizeHalf_ * sign ),roomGraphHandle_,true);
 		break;
 	case LOCK_ROOM:
-		DrawGraph(x + blockSizeHalf_,y + blockSizeHalf_,lockroomGraphHandle,true);
+		DrawGraph(x + ( blockSizeHalf_ * sign ),y + ( blockSizeHalf_ * sign ),lockroomGraphHandle_,true);
 		break;
 	default:
 		break;
