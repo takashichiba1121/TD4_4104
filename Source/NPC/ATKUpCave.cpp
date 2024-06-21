99b838046d5926fc537289d5e0931ea77c5641db
楓 紅桜 <rngkq545@gmail.com>
2024-06-21 16:52:54 +0900

    push

--- Source/NPC/ATKUpCave.cpp
+++ Source/NPC/ATKUpCave.cpp
@@ -12,27 +12,5 @@ void ATKUpCave::Initialize()
 
 bool ATKUpCave::StatusChenge()
 {
-	PowerUp* product = &products_[selectNum_];
-	switch (magic_enum::enum_cast< Status >( product->statusNames.second ).value())
-	{
-	case HP:
-		playerPtr_->SubMaxHp(product->cost);
-		break;
-	case ATK:
-		playerPtr_->SubPow(product->cost);
-		break;
-	case DEF:
-		playerPtr_->SubDef(product->cost);
-		break;
-	case SPD:
-		playerPtr_->SubSpd(product->cost);
-		break;
-	case CRIT:
-
-		break;
-	case CDMG:
-		break;
-	default:
-		break;
-	}
+	
 }
