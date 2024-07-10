1198653bae51c1c7750978cc65be54c7d1605f37
楓 紅桜 <rngkq545@gmail.com>
2024-07-10 11:38:21 +0900

    PowerUp

--- Include/Scene/GameScene.h
+++ Include/Scene/GameScene.h
@@ -6,6 +6,7 @@
 #include"Player.h"
 #include "EnemyManager.h"
 #include"CollisionManager.h"
+#include "PowerUpCave.h"
 
 #include"MapChip.h"
 
@@ -17,6 +18,7 @@ private:
 	std::unique_ptr<EnemyManager> enemys_;
 
 	std::unique_ptr<MapChip>mapChip_;
+	std::unique_ptr<PowerUpCave>powerUp_;
 
 	uint32_t backGround_;
 
