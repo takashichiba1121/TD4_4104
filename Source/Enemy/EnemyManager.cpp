721d48643d2b1677d830cf0438767c29395f6ad8
maple-c-blossom <90586502+maple-c-blossom@users.noreply.github.com>
2024-08-04 05:12:03 +0900

    Texコメント

--- Source/Enemy/EnemyManager.cpp
+++ Source/Enemy/EnemyManager.cpp
@@ -273,17 +273,17 @@ int32_t EnemyManager::GetSoundHandle(std::string name)
 
 void EnemyManager::TexLoad()
 {
-	texs_[ "fly" ] = LoadGraph(string("Resources\\Enemy\\enemyFly.png"));
+	texs_[ "fly" ] = LoadGraph(string("Resources\\Enemy\\enemyFly.png"));//飛行
 
-	texs_[ "shootMove" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));
+	texs_[ "shootMove" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));//遠距離移動
 
-	texs_[ "shootAttack" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));
+	texs_[ "shootAttack" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));//遠距離攻撃
 
-	texs_[ "adjacentMove" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));
+	texs_[ "adjacentMove" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));//近距離移動
 
-	texs_[ "adjacentAttack" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));
+	texs_[ "adjacentAttack" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));//近距離攻撃
 
-	texs_[ "adjacentDash" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));
+	texs_[ "adjacentDash" ] = LoadGraph(string("Resources\\Player\\PlayerDush.png"));//近距離ダッシュ(プレイヤーに接近)
 
 
 }
