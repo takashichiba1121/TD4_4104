d808ccc0628e32e92c746ff5f9193e4219d70492
keita_momiyama <93181795+keita034@users.noreply.github.com>
2024-08-03 23:50:31 +0900

    サウンドマネージャー追加

--- Include/Util/DxlibInclude.cpp
+++ Include/Util/DxlibInclude.cpp
@@ -1,7 +1,13 @@
 #include "DxlibInclude.h"
 #include<TextureManager.h>
+#include<SoundManager.h>
 
 int LoadGraph(const std::string& FileName)
 {
     return TextureManager::Load(FileName);
+}
+
+int LoadSoundMem(const std::string& FileName)
+{
+    return SoundManager::Load(FileName);
 }
